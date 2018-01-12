f304dcfe92f97baad57d013f71dbf88c2de6f82c
kunal52 <purikunal22@gmail.com>
2018-01-12 18:54:39 +0530

    First

--- app/src/main/java/com/techweblearn/githubtest/MainActivity.java
+++ app/src/main/java/com/techweblearn/githubtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.techweblearn.githubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG","TAG");
     }
 }
